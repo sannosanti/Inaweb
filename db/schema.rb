e97f6bd6ee2cb627f21f2b39550452a313b0d24c
danie-1123 <daniel1123otalora@gmail.com>
2016-12-16 23:34:21 +0600

    instalacion postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161214204234) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "documents", force: :cascade do |t|
     t.string   "titulo"
     t.integer  "categoria"
@@ -44,9 +47,10 @@ ActiveRecord::Schema.define(version: 20161214204234) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "role_id"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["role_id"], name: "index_users_on_role_id"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["role_id"], name: "index_users_on_role_id", using: :btree
   end
 
+  add_foreign_key "users", "roles"
 end
